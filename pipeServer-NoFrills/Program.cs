a547d674cbe8f42cccc88be14e856ef45a501eae
mar Jinn <marjinn@live.com>
2013-03-03 06:32:37 +0530

    A simple Named Pipe Server
    listening for clients is in  a "while (true)" loop
    reading from buffer is  also in   a "while (true)" loop
    
    CreateNamedPipe
    ConnectNamedPipe
    
    both inside a while (true) loop
    
    buffer reading in another while (true)loop inside the first
    active only when the ConnectNamedPipe function returns anything but 1 -
    meaning is successful
    
    reply message has been set  to send the text "message" to the same loop

---
+++ pipeServer-NoFrills/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace pipeServer_NoFrills
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Server ServTest = new Server();
+           Thread T = new Thread(ServTest.Serveradd);
+           T.Name = @"ServTest.Serveradd";
+           T.Start();
+
+           Console.WriteLine("mammama");
+            
+            Console.ReadLine();
+        }
+
+
+       
+        
+            
+        
+
+
+    
+    }
+
+
+}
---
+++ pipeServer-NoFrills/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pipeServer-NoFrills")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pipeServer-NoFrills")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f7eddb0-c1b2-4a31-ad91-647deff51349")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
